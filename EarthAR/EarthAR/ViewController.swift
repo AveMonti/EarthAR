7a772652beb7e1688a3a60f3812e373ddbac1bf9
Mateusz Chojnacki <mateuszchojnacki@MacBook-Air-Mateusz.local>
2018-02-26 21:27:48 +0100

    add moon

--- EarthAR/EarthAR/ViewController.swift
+++ EarthAR/EarthAR/ViewController.swift
@@ -17,17 +17,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.session.run(configuration)
+        self.addNode()
     }
     
     func addNode(){
-        let node = SCNNode()
-        node.position = SCNVector3(0.1,0.1,0.1) // Meters
-        node.geometry = SCNSphere(radius: 0.3)
-        node.geometry?.firstMaterial?.diffuse.contents = UIImage(contentsOfFile: "normal")
-        node.geometry?.firstMaterial?.specular.contents = UIImage(contentsOfFile: "specular")
+        let earthNode = SCNNode()
+        earthNode.position = SCNVector3(0.1,0.1,0.1) // Meters
+        earthNode.geometry = SCNSphere(radius: 0.3)
+        earthNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "earth_daymap")
+        earthNode.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "earth_specular_map")
         
         self.sceneView.automaticallyUpdatesLighting = true
-        self.sceneView.scene.rootNode.addChildNode(node) 
+        self.sceneView.scene.rootNode.addChildNode(earthNode)
+        
+        
+        let moonNode = SCNNode()
+        moonNode.position = SCNVector3(0,0,1) // Meters
+        moonNode.geometry = SCNSphere(radius: 0.2)
+        moonNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "moon")
+        
+        earthNode.addChildNode(moonNode)
+        
+        let rotateAction = SCNAction.rotateBy(x: 0,
+                                              y: CGFloat(Double.pi * 2),
+                                              z: 0,
+                                              duration: 10)
+        earthNode.runAction(SCNAction.repeatForever(rotateAction))
+        
+        
     }
 
 
